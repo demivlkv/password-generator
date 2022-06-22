508c9f407afb7c623bf18fc48c476e8d092cae01
Demi Hayashi <demi.h@me.com>
2022-06-22 15:40:22 -0700

    solved issue within for loop

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-    console.log("Testing, testing!");
-
     // list arrays of all characters & symbols available to use to generate password
     var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -13,7 +11,7 @@ function generatePassword() {
     // minimum password character count
     var characterCount = 8;
     var userChoice = [];
-    var passwordArr = [];
+    var choiceArray = [];
 
     // ask the user for their desired password length
     var characterCount = window.prompt("How many characters would you like your password to be? Please choose a length between 8 and 128.");
@@ -25,10 +23,10 @@ function generatePassword() {
     }
 
     // list of user input variables
-    var confirmLowercase = window.confirm("Would you like to include lowercase letters in the password?");
-    var confirmUppercase = window.confirm("Would you like to include uppercase letters in the password?");
-    var confirmNumbers = window.confirm("Would you like to include numbers in the password?");
-    var confirmSymbols = window.confirm("Would you like to include special characters in the password?");
+    var confirmLowercase = window.confirm("Would you like to include lowercase letters in your password?");
+    var confirmUppercase = window.confirm("Would you like to include uppercase letters in your password?");
+    var confirmNumbers = window.confirm("Would you like to include numbers in your password?");
+    var confirmSymbols = window.confirm("Would you like to include special characters in your password?");
 
     // include lowercase, uppercase, and/or special characters
     if (confirmLowercase) {
@@ -48,14 +46,14 @@ function generatePassword() {
     console.log(userChoice);
 
     // generate password based on user criteria
-    var password = "";
+    var newPassword = "";
     for (var i = 0; i < characterCount; i++) {
-        userInput = Math.floor(Math.random() * userChoice.length);
-        password = password + passwordArr[userInput];
+        var randomChar = Math.floor(Math.random() * userChoice.length);
+        newPassword = newPassword + userChoice[randomChar];
     }
 
     // display generated password!
-    return password;
+    return newPassword;
 }
 
 // Write password to the #password input
