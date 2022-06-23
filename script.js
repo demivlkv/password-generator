79c02c82ea9b7537ddeae6913daa6874baa9ae5f
Demi Hayashi <demi.h@me.com>
2022-06-23 09:46:54 -0700

    cleaned up js file

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ function generatePassword() {
     var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     var symbol = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "~"];
-
-    // minimum password character count
-    var characterCount = 8;
     var userChoice = [];
 
     // ask user for their desired password length
